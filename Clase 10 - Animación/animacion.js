3412aab3fdda1bd21abf22e6d8e59f9d68c25d4f
Luis Carlos Castro <gigicastroc2003@gmail.com>
2020-11-25 19:05:54 -0500

    Canvas con dos objetos

--- Clase 10 - Animación/animacion.js
+++ Clase 10 - Animación/animacion.js
@@ -1,18 +1,41 @@
 let terza = document.querySelector("#terza");
+let nombre = document.querySelector("#Luis");
 
 function cambiarColor() {
   terza.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  nombre.style.color = "#" + Math.floor(Math.random() * 16777215).toString(16);
 }
 // setInterval> ejecuta una función cada cierto tiempo. El tiempo se debe escribir en milisegundos
 let x = 0;
-let dX = 10;
+let y = 0;
+let dX = 15;
+let dY = 5;
 let angle = 0;
 let dAngle = 5;
-function mover() {
-  x = x + dX;
+let t = 0;
+let x2 = 10;
+let y2 = 500;
+let y20 = 449;
+let vX = 0.1;
+let vY = 1;
+
+function mover(){
+  {
+    x = x + dX;
+    y = y +dY;
+    angle = angle + dAngle;
+    terza.style.transform = `translate(${x}px, ${y}px) rotate(${angle}deg)`;
+  }
+  {
+  t+= 0.1;
+  x2 = x2 + vX*t;
+  y2 = y20 + vY* vY*t + (9.8*Math.pow(t,2))/2;
   angle = angle + dAngle;
-  terza.style.transform = `translate(${x}px) rotate(${angle}deg)`;
+  nombre.style.transform = `translate(${x2}px, ${y2}px) rotate(${angle}deg)`
+  }
+
 }
+
 let idMovimiento;
 let idColor;
 let corriendo = false;
